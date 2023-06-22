b3a177d6b160e54fc1f478c5be28a0539c3df28c
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2023-06-22 17:20:05 +0200

    Added formbutton

---
+++ docs/assets/js/formbutton.js
@@ -0,0 +1,31 @@
+/* paste this line in verbatim */
+window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+/* customize formbutton below*/
+formbutton("create", {
+  action: "https://formspree.io/f/xqkonqqq",
+  title: "How can we help?",
+  fields: [
+    {
+      type: "email",
+      label: "Email:",
+      name: "email",
+      required: true,
+      placeholder: "your@email.com"
+    },
+    {
+      type: "textarea",
+      label: "Message:",
+      name: "message",
+      placeholder: "What's on your mind?",
+    },
+    { type: "submit" }
+  ],
+  styles: {
+    title: {
+      backgroundColor: "#ff7518"
+    },
+    button: {
+      backgroundColor: "#ff7518"
+    }
+  }
+});
